b57463483d91b1dff5876edec2bee7741f599a7f
Amir Nasser <ANasser@whitecapcanada.com>
2021-11-05 11:31:06 -0400

    Update

--- webdeploy.php
+++ webdeploy.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Webneat Deploy
- * Version: 1.0.0
+ * Version: 1.1.0
  * Plugin URI: http://www.webneat.ca/webneat-deploy
  * 
  * Description: This plugins helps system addmin deployment easy.
